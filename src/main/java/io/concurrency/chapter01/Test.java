c8fdfac8763841535b1f4745a0d6e3f9bac6292e
kihyuk.jeong <kihyuk.jeong@musinsa.com>
2024-03-31 22:24:49 +0900

    Thread Fundamental - exception, step

--- src/main/java/io/concurrency/chapter01/Test.java
+++ src/main/java/io/concurrency/chapter01/Test.java
@@ -1,11 +1,35 @@
 package io.concurrency.chapter01;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Test {
     public static void main(String[] args) {
 
-        Temp1.VALUES[0] = 10;
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
+        // Task 1 - Search
+        Runnable searchTask = () -> {
+            System.out.println("start 1");
+            // Call your search method here
+        };
 
-        System.out.println( Temp1.VALUES[0] );
+        // Task 2 - Delete
+        Runnable deleteTask = () -> {
+            // Call your delete method here
+            System.out.println("start 2");
+
+        };
+
+        Runnable deleteTask2 = () -> {
+            // Call your delete method here
+            System.out.println("start 3");
+
+        };
+
+        // Execute both tasks concurrently
+        executor.submit(searchTask);
+        executor.submit(deleteTask);
+        executor.submit(deleteTask2);
     }
 }
